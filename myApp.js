f6bbbe20ea33ded8606117dd84af9ca9171a2a21
jimhome_mac <jsr1611@outlook.com>
2023-06-01 08:47:59 +0900

    basic node api

--- myApp.js
+++ myApp.js
@@ -1,12 +1,36 @@
 let express = require('express');
 let app = express();
 
+// print text to console 
+console.log("Hello World");
 
 
+// basic express api
+app.get("/", function(req, res){
+    res.send("Hello Express");
+});
 
 
+// Serve an HTML File
+const filePath = __dirname + "/views/index.html";
+app.get("/index", function(req, res){
+    res.sendFile(filePath);
+});
 
 
+// Serve Static Assets
+const middleWare = express.static(__dirname+"/public");
+app.use("/public", middleWare);
+
+
+// Serve JSON on a Specific Route
+const msg = {message: "Hello JSON"};
+app.get("/json", function(req,res){
+    res.json(msg);
+});
+
+
+// Use the .env File
 
 
 
