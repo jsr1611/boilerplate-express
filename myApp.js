c62df1e32ef9b51316a11519ca9ca0fac5ad89a1
jimhome_mac <jsr1611@outlook.com>
2023-06-02 00:50:06 +0900

    added some API basics such as body-parser to read POST data from client, chaining get and post routes, etc.

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 let express = require("express");
+let bodyParser = require('body-parser');
 let app = express();
 
 // print text to console
 console.log("Hello World");
+
 
 // Implement a Root-Level Request Logger Middleware
 // should be mounted before other API endpoint declarations to apply the logger
@@ -12,6 +14,10 @@ const Logger = function (req, res, next) {
     next();
 };
 app.use(Logger);
+
+// Use body-parser to Parse POST Requests
+// With Ajax, you can also use JSON to handle data having a more complex structure. Another type of encoding: multipart/form-data is used to upload binary files. 
+app.use(bodyParser.urlencoded({extended: false}));
 
 // basic express api
 app.get("/", function (req, res) {
@@ -39,5 +45,45 @@ app.get("/json", function (req, res) {
     }
     res.json({ message: msg });
 });
+
+
+// Chain Middleware to Create a Time Server
+
+app.get("now", function(req, res, next){
+    req.time = new Date().toString();
+    next();
+}, function(req, res){
+    res.json({time: req.time});
+});
+
+// Get Route Parameter Input from the Client
+
+app.get("/:word/echo", function(req, res){
+    res.json({echo: req.params.word});
+});
+
+// Get Query Parameter Input from the Client
+
+// to receive data from a POST request, at the same /name route path, use the method app.route(path).get(handler).post(handler). 
+// This syntax allows you to chain different verb handlers on the same path route. You can save a bit of typing, and have cleaner code.
+
+// app.get("/name", function(req, res){
+//   let name = req.query.first + " " + req.query.last;
+//   res.json({name: name});
+// });
+
+// Get Data from POST Requests
+// If the body-parser is configured correctly, you should find the parameters in the object req.body.
+
+app.route("/name").get(function(req, res){
+    let name = req.query.first + " " + req.query.last;
+    res.json({name: name});
+}).post(function(req, res){
+    let name = req.body.first + " " + req.body.last;
+    res.json({name: name});
+});
+
+
+
 
 module.exports = app;
